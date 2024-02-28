61096c17206ded7f51c16f80a557cd9f551e2b94
Griffith(Jeremiah) <145460979+SneakyNutt@users.noreply.github.com>
2024-02-28 10:25:03 -0500

    Whats going On

---
+++ Exercise 8/Exercise 8/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Exercise_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxnumber;
+            //print all even #s from 0 to the maximum #
+            Console.WriteLine("Enter a Maximum  number (1-15)");
+            maxnumber= int.Parse(Console.ReadLine());
+
+            //print even #s from 1-Max(15)
+            for (int i = 0; i < maxnumber; i++) ;
+        }
+    }
+}
