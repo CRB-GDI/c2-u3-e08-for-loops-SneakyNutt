145d92b6728c7857acfc71c29f3320d1c0a54f97
Griffith(Jeremiah) <145460979+SneakyNutt@users.noreply.github.com>
2024-02-29 09:12:36 -0500

    Finished 8

--- Exercise 8/Exercise 8/Program.cs
+++ Exercise 8/Exercise 8/Program.cs
@@ -4,13 +4,28 @@
     {
         static void Main(string[] args)
         {
-            int maxnumber;
-            //print all even #s from 0 to the maximum #
-            Console.WriteLine("Enter a Maximum  number (1-15)");
-            maxnumber= int.Parse(Console.ReadLine());
-
+           //print all even #s from 0 to the maximum #
+           Console.WriteLine("Enter a Maximum  number");
+           int maxnumber;
+           int printednumber = 0;
+          
+           maxnumber= int.Parse(Console.ReadLine());
+            
             //print even #s from 1-Max(15)
-            for (int i = 0; i < maxnumber; i++) ;
+            for (printednumber = 0; printednumber <=maxnumber; printednumber +=2) 
+            { 
+                Console.WriteLine(printednumber);
+            }
+            Console.WriteLine("Enter a Row Width");
+            int rowwidth;
+            rowwidth = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= rowwidth; i++)
+            {
+                Console.Write("*");
+            }
+            
+            
+            
         }
     }
 }
